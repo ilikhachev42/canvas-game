f45e75849ab5055129fbb36a1eb4d97143498bd2
justkoi42 <justkoi42@gmail.com>
2023-04-17 21:26:35 +0530

    add a Sprite class for sprite animations

---
+++ js/classes/Sprite.js
@@ -0,0 +1,65 @@
+class Sprite {
+  constructor({
+    position = { x: 0, y: 0 }, 
+    imageSrc, 
+    frames = {  x: 1, y: 1 }
+  }) {
+    this.position = position
+    this.image = new Image()
+    this.image.src = imageSrc
+    this.framesX = {
+      max: frames.x,
+      current: 0,
+      elapsed: 0,
+      hold: 3
+    }
+    this.framesY = {
+      max: frames.y,
+      current: 0
+    }
+    this.waypointIndex = 0;
+  }
+
+  draw() {
+    const cropWidth = this.image.width / this.framesX.max
+    const cropHeight = this.image.height / this.framesY.max
+    const crop = {
+      position: {
+        x: cropWidth * this.framesX.current,
+        y: cropHeight * this.framesY.current
+      },
+      width: cropWidth,
+      height: cropHeight
+    }
+    
+    ctx.drawImage(
+      this.image, 
+      crop.position.x, 
+      crop.position.y,
+      crop.width,
+      crop.height,
+      this.position.x - 32,
+      this.position.y,
+      crop.width,
+      crop.height
+    )
+  }
+
+  update() {
+    this.framesX.elapsed++
+    if (this.framesX.elapsed % this.framesX.hold === 0) {
+      this.framesX.current++
+      if (this.framesX.current >= this.framesX.max) {
+        this.framesX.current = 0;
+      }
+    }
+  }
+
+ // function for animating sprites ONLY for Enemy class.
+  enemyUpdate() {
+    this.framesX.current++;
+    if (this.framesX.current >= this.framesX.max) {
+      this.framesX.current = 0;
+    }
+  }
+}
